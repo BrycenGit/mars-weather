c35c5a62afd15721077e32365c236eaa79d2ed94
Garrett Brown <garrettpaulbrown@gmail.com>
2020-09-09 11:19:56 -0700

    Update CSS to background red/orange. co-authored-by: Brycen Bartolome <brycenbartolome@gmail.com>

--- src/main.js
+++ src/main.js
@@ -3,6 +3,18 @@ import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
 
+function displaySolKeys(response) {
+let solKey;
+solKey = response.sol_keys[0]
+return solKey;
+}
+
+function displayDate(response) {
+  let earthDate;
+  earthDate = response.[634].First_UTC;
+  console.log(earthDate);
+}
+
 $(document).ready(function() {
   let request = new XMLHttpRequest();
   const url = `https://api.nasa.gov/insight_weather/?api_key=${process.env.API_KEY}&feedtype=json&ver=1.0`
@@ -10,6 +22,8 @@ $(document).ready(function() {
     if (this.readyState === 4 && this.status === 200) {
       const response = JSON.parse(this.responseText);
       console.log(response);
+      displaySolKeys(response);
+      displayDate(response);
     }
   };
   request.open("GET", url, true);
